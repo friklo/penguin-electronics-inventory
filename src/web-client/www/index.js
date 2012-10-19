8a6d68b721ec41a6ca0b191c16ce9810b0c96506
Zachary <zsclapper7@gmail.com>
2012-10-19 12:43:37 -0400

    Started implementation of category selection/display in the main window. Category list display changes (style and alphabetization).

--- src/web-client/www/index.js
+++ src/web-client/www/index.js
@@ -1,3 +1,24 @@
+var idPrefix = "cat";
+
+function sortByObjectName(a, b)
+{
+	var nameA = a.name;
+	var nameB = b.name;
+	
+	if(nameA < nameB)
+	{
+		return -1;
+	}
+	else if(nameB < nameA)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
 $(document).ready(
 	function()
 	{
@@ -7,7 +28,7 @@ $(document).ready(
 		
 		$(window).resize(resizeContainer);
 		
-		$("#listCategoriesButton").click(listCategories);
+		listCategories();
 	}
 );
 
@@ -30,7 +51,10 @@ function displayCategories(data)
 {
 	var output = "<ul class='collapsibleList'>";
 	
-	console.log(data);
+	// Alphabetize the list
+	data.cats.sort(sortByObjectName);
+	
+	console.log(data.cats);
 	
 	for(i = 0; i < data.cats.length; i++)
 	{
@@ -40,13 +64,24 @@ function displayCategories(data)
 	output += "</ul>";
 	
 	CollapsibleLists.applyTo($("#categoryList").html(output)[0]);
+	
+	$("span.categoryLabel").unbind("click").click(
+		function(event)
+		{
+			var id = $(event.target).parent().attr("id").substring(idPrefix.length);
+			
+			$("#tabContainer").html(displayCategory(getCategoryByID(id, data.cats)));
+		}
+	);
 }
 
 function createRecursiveList(category)
 {
-	var output = "<li>";
+	var output = "<li id='" + idPrefix + category.id + "'>";
 	
-	output += category.name;
+	output += "<span class='categoryLabel'>" + category.name + "</span>";
+	
+	category.children.sort(sortByObjectName);
 	
 	if(category.children.length > 0)
 	{
@@ -61,4 +96,42 @@ function createRecursiveList(category)
 	}
 	
 	return output;
+}
+
+function getCategoryByID(id, categories)
+{
+	for(var h = 0; h < categories.length; h++)
+	{
+		if(categories[h].id == id)
+		{
+			return categories[h];
+		}
+		else if(categories[h].children.length > 0)
+		{
+			var category = getCategoryByID(id, categories[h].children);
+			
+			// If it's not null
+			if(category)
+			{
+				return category;
+			}
+		}
+	}
+	
+	return null;
+}
+
+function displayCategory(category)
+{
+	console.log(category);
+	
+	if(category)
+	{
+	
+		return "<span>You selected '" + category.name + "'</span>";
+	}
+	else
+	{
+		return "Error in 'dislayCategory', null category";
+	}
 }
